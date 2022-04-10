227cc1170acac8c6198f54ba72ff613315d34d21
loressl <loryssl@hotmail.com>
2022-04-09 22:15:56 -0300

    Adicionado Header e estilo para o post na home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,10 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head'
+import Link from 'next/link'
 
 import { getPrismicClient } from '../services/prismic';
+
+import { FiCalendar, FiUser } from 'react-icons/fi'
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
@@ -24,9 +28,31 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | spacetraveling</title>
+      </Head>
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <Link href="#">
+            <a>
+              <strong>Como utilizar Hooks</strong>
+              <p>Pensando em sincronização em vez de ciclos de vida.</p>
+              <div className={styles.info}>
+                <FiCalendar />
+                <time>10 abr 2022</time>
+                <FiUser />
+                <p>Joseph Oliveira</p>
+              </div>
+            </a>
+          </Link>
+        </div>
+      </main>
+    </>
+  )
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
